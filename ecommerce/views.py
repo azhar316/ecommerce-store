a6624d141576737058b925e2172389436f30f8f7
Azharuddin Syed <sayed.azharudin@gmail.com>
2020-04-04 16:35:51 +0530

    fix:latest product algorithm

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -5,7 +5,7 @@ from products.models import Product
 
 
 def index(request):
-    latest_products = Product.objects.order_by('time_stamp')[:5]
+    latest_products = Product.objects.order_by('-time_stamp')[:5]
     discounted_products = Product.objects.order_by('-discount_percentage')
     products = {'latest_products': latest_products, 'discounted_products': discounted_products}
     return render(request, 'ecommerce/index.html', products)
