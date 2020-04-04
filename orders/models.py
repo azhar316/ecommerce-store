7d1c4a3d09a6eee6bc3fe031d008ea7869e56406
Azharuddin Syed <sayed.azharudin@gmail.com>
2020-04-04 12:56:41 +0530

    design change:Modify Order model

--- orders/models.py
+++ orders/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.conf import settings
 from django.utils import timezone
 
-from carts.models import ProductEntry
+from products.models import Product
 from addresses.models import Address
 
 
@@ -24,13 +24,15 @@ ORDER_TRACKING_CHOICES = (
 class Order(models.Model):
 
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
-    product_entries = models.ManyToManyField(ProductEntry)
+    product = models.ForeignKey(Product, on_delete=models.DO_NOTHING)
+    quantity = models.PositiveIntegerField()
     address = models.ForeignKey(Address, on_delete=models.DO_NOTHING)
     tracking = models.CharField(max_length=120, choices=ORDER_TRACKING_CHOICES)
     status = models.CharField(max_length=120, choices=ORDER_STATUS_CHOICES)
     subtotal = models.DecimalField(default=0.0, max_digits=10, decimal_places=2)
     total = models.DecimalField(default=0.0, max_digits=10, decimal_places=2)
-    timestamp = models.DateTimeField(default=timezone.now)
+    ordered_date = models.DateTimeField(default=timezone.now)
+    delivered_date = models.DateTimeField()
 
     def __str__(self):
         return self.pk
